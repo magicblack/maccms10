f102942107849062e3432f90a6b6a6bc1d2f7054
magicblack <479025@qq.com>
2019-07-13 13:15:59 +0800

    更新版本号

--- application/extra/version.php
+++ application/extra/version.php
@@ -3,7 +3,7 @@ return array (
     'name' => '苹果CMS',
     'copyright' => 'MacCMS.COM',
     'url' => 'http://www.maccms.com/',
-    'code' => '2019.1000.1001',
+    'code' => '2019.1000.1002',
     'license' => '免费版',
 );
 ?>
