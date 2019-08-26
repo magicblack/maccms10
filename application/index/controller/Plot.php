1b5297dd8a2a3a692d24806daaaa805021b56940
magicblack <479025@qq.com>
2019-08-26 22:46:25 +0800

    缺少的分集剧情文件

---
+++ application/index/controller/Plot.php
@@ -0,0 +1,51 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+
+class Plot extends Base
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        return $this->label_fetch('plot/index');
+    }
+
+    public function search()
+    {
+        $param = mac_param_url();
+        $this->check_search($param);
+        $this->assign('param',$param);
+        return $this->label_fetch('plot/search');
+    }
+
+    public function ajax_search()
+    {
+        $param = mac_param_url();
+        $this->check_search($param);
+        $this->assign('param',$param);
+        return $this->label_fetch('plot/ajax_search');
+    }
+
+    public function detail()
+    {
+        $info = $this->label_vod_detail();
+        return $this->label_fetch('plot/detail');
+    }
+
+    public function ajax_detail()
+    {
+        $info = $this->label_vod_detail();
+        return $this->label_fetch('plot/ajax_detail');
+    }
+
+    public function rss()
+    {
+        $info = $this->label_vod_detail();
+        return $this->label_fetch('plot/rss');
+    }
+
+}
