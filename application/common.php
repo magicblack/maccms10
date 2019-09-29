7bc026e37aadf7f642d0638a4f194ac70eee90e7
magicblack <479025@qq.com>
2019-09-29 16:37:02 +0800

    紧急修复上次更新造成的采集报错的问题。

--- application/common.php
+++ application/common.php
@@ -768,8 +768,8 @@ function mac_rep_pse_syn($psearr,$txt)
 }
 
 function mac_get_tag($title,$content){
-    $url ='http://api.maccms.com/keyword/?callback=&txt='.rawurlencode($title).rawurlencode(substring(strip_tags($content),200));
-    $data = getPage($url,'utf-8');
+    $url ='http://api.maccms.com/keyword/?callback=&txt='.rawurlencode($title).rawurlencode(mac_substring(strip_tags($content),200));
+    $data = mac_curl_get($url);
 	$json = @json_decode($data,true);
 	if($json){
 		if($json['code']==1){
