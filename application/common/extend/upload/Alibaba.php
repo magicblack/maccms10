b6a7befb7a1c822bfbd45ea0abef459472eafb07
刘明野 <898310895@qq.com>
2020-09-03 17:33:18 +0800

    修复阿里巴巴图床

--- application/common/extend/upload/Alibaba.php
+++ application/common/extend/upload/Alibaba.php
@@ -12,7 +12,7 @@ class Alibaba
 
         $url = 'https://kfupload.alibaba.com/mupload';
         $data = [];
-        $data['scene'] = 'aeMessageCenterV2ImageRule';
+        $data['scene'] = 'aeMessageCenterImageRule';
         $data['name'] = 'player.jpg';
         if (class_exists('CURLFile')) {
             $data['file'] = new \CURLFile(realpath($file_path));
