f0d88e70cb712970f7b81d588b2cb491c63f8bd8
magicblack <maccms4@protonmail.com>
2021-09-24 13:07:13 +0800

    update to 3023

--- application/extra/version.php
+++ application/extra/version.php
@@ -3,7 +3,7 @@ return array (
     'name' => '苹果CMS内容管理系统',
     'copyright' => 'MacCMS',
     'url' => '//github.com/magicblack',
-    'code' => '2022.1000.3022',
+    'code' => '2022.1000.3023',
     'license' => '开源版',
 );
 ?>
