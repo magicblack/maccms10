2208631df945920c4a5cc5b47b672972e8998710
magicblack <20452515+magicblack@users.noreply.github.com>
2022-03-19 21:52:43 +0800

    fix #660 版权处理允许跳转链接为空

--- application/index/controller/Vod.php
+++ application/index/controller/Vod.php
@@ -55,7 +55,7 @@ class Vod extends Base
     public function detail()
     {
         $info = $this->label_vod_detail();
-        if($info['vod_copyright']==1 && !empty($info['vod_jumpurl']) && $GLOBALS['config']['app']['copyright_status']==2){
+        if($info['vod_copyright']==1 && $GLOBALS['config']['app']['copyright_status']==2){
             return $this->label_fetch('vod/copyright');
         }
         if(!empty($info['vod_pwd']) && session('1-1-'.$info['vod_id'])!='1'){
