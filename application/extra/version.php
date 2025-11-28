0f71045f3cdc583074b996e3f36345f42e59f126
magicblack <20452515+magicblack@users.noreply.github.com>
2025-11-28 09:46:54 +0800

    升级版本至 2025.1000.4052

--- application/extra/version.php
+++ application/extra/version.php
@@ -3,7 +3,7 @@ return array (
     'name' => '苹果CMS内容管理系统',
     'copyright' => 'MacCMS',
     'url' => '//github.com/magicblack',
-    'code' => '2025.1000.4051',
+    'code' => '2025.1000.4052',
     'license' => '开源版',
 
 );
