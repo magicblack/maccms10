d95f9e220d205cbe18e986ea5b8d119a125205fe
magicblack <20452515+magicblack@users.noreply.github.com>
2026-05-27 23:27:20 +0800

    fix #1351 重生成 update_hash 修复后台核心文件校验拦截登录

--- application/extra/version.php
+++ application/extra/version.php
@@ -5,6 +5,6 @@ return array (
     'url' => '//github.com/magicblack',
     'code' => '2026.1000.4053',
     'license' => '开源版',
-    'update_hash' => '44cdf9a65d340290d4fcc236a3bdfb88',
+    'update_hash' => '91150bcbcbcbd2532bbe3911d43e4a8f',
 );
 ?>
