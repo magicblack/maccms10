507be8bc6cc93408647979398e124aaa553afa2e
magicblack <20452515+magicblack@users.noreply.github.com>
2026-08-19 13:35:20 +0800

    maccms10 更新至 v2026.1000.4056

--- application/extra/version.php
+++ application/extra/version.php
@@ -6,7 +6,7 @@ return array (
     'name' => '苹果CMS内容管理系统',
     'copyright' => 'MacCMS',
     'url' => '//github.com/magicblack',
-    'code' => '2026.1000.4055',
+    'code' => '2026.1000.4056',
     'license' => '开源版',
     'update_hash' => '4d95c1cdb0133a17a4e482c72aff430d',
 );
